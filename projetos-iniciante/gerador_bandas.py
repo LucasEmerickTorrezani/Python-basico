2291e68de7c472bb87d306fbe4623ef9c4e86c78
LucasEmerickTorrezani <lucas.emerick.t@gmail.com>
2025-10-16 15:15:05 -0300

    Update gerador_bandas.py
    # Gerador de Nomes de Bandas 
    Meu primeiro projeto Python! Um gerador simples de nomes criativos para bandas.
    
    ## Como usar
    1. Execute o script
    2. Digite o nome da sua cidade
    3. Digite o nome do seu pet
    4. Receba sugestões de nomes!

--- projetos-iniciante/gerador_bandas.py
+++ projetos-iniciante/gerador_bandas.py
@@ -1,1 +1,4 @@
-
+print('Bem vindo ao idealizador de nomes de banda de rock!!!')
+cidade = input('Qual o nome da sua cidade?\n')
+pet= input('O nome do seu pet?\n')
+print(f'O nome da sua banda poderia ser:{cidade +' '+ pet}')
